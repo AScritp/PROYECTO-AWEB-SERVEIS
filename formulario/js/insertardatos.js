64847d52721dfdb2a1d18269fbed0aab14121a73
nataliagamezbarea <144036430+nataliagamezbarea@users.noreply.github.com>
2023-12-01 12:01:12 +0000

    FORM

--- formulario/js/insertardatos.js
+++ formulario/js/insertardatos.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
-    $('#formulario-pago').submit(function(event){
-      event.preventDefault();
-      SendFormGoogleSheets();
-    });
+  $('#formulario-pago').submit(function(event){
+    event.preventDefault();
+    SendFormGoogleSheets();
   });
+});
 
-  function SendFormGoogleSheets() {
-    $.ajax({
-      url: 'https://script.google.com/macros/s/AKfycbxRPc0QevvIPaXdorzDD8_z6laBDQ1r90LdZts9V61A8iwX-oYFPRVg-YlosIEpgYfNsA/exec',
-      type: 'post',
-      data: $('#formulario-pago').serializeArray(),
-      success: function(){
-        alert("Registro exitoso")
-      },
-      error: function(){
-        alert("Error en el Registro :(")
-      }
-    });
-  }
+function SendFormGoogleSheets() {
+  $.ajax({
+    url: 'https://script.google.com/macros/s/AKfycbxAlknHO-trrF_zEwHb7k74sbzjOuuWLUn0HQeUH91tHRQv0UByybklShIkEzchhXCn0Q/exec',
+    type: 'post',
+    data: $('#formulario-pago').serializeArray(),
+    success: function(){
+      alert("Registro exitoso")
+    },
+    error: function(){
+      alert("Error en el Registro :(")
+    }
+  });
+}
